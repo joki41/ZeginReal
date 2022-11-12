5fe35302c4139dc45e98188abafc524daefa9cd3
joki41 <jordan41ru@gmail.com>
2022-11-12 11:56:40 +0100

    Initial commit

---
+++ ZeginReal/Strana1.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ZeginReal
+{
+    internal class Strana1
+    {
+        IWebDriver driver = null;
+        public Strana1(IWebDriver driver)
+        {
+            this.driver = driver;
+            driver.Navigate().GoToUrl("https://www.zegin.com.mk/mk");
+            driver.Manage().Window.Maximize();
+
+            //find kopce ok kukisi i kopce od reklama i klikni
+           
+        }
+        public void NapisiTekstVoPoleto(string tekst)
+        {
+            driver.Navigate().GoToUrl("https://www.zegin.com.mk");
+            driver.FindElement(By.XPath("//input[@type='text']")).Click();
+            driver.FindElement(By.XPath("//input[@type='text']")).Clear();
+            driver.FindElement(By.XPath("//input[@type='text']")).SendKeys("Sirup");
+            driver.FindElement(By.XPath("//input[@type='text']")).SendKeys(Keys.Enter);
+
+            //
+        }
+        public void SocialMediaButtons1()
+        {
+             
+
+
+
+
+        }
+    }
+}
---
+++ ZeginReal/UnitTest1.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeginReal
+{
+    [TestClass]
+    [TestCategory("POM")]
+    public class UnitTest1
+    {
+        static IWebDriver driver = new ChromeDriver();
+        Strana1 homepage = new Strana1(driver);
+        [TestMethod]
+        public void TestMethod1()
+        {
+            homepage.NapisiTekstVoPoleto("Sirup");
+
+            Thread.Sleep(3000);
+
+            string strurl = driver.Url;
+
+            Assert.AreEqual("https://www.zegin.com.mk/mk/products?product=Sirup", strurl);
+        }
+        [TestMethod]
+        public void MedijaKopcinja()
+        {
+           
+        }
+
+
+    }
+}
